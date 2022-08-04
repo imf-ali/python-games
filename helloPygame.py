998c517a48505d0a73d08a7ac19dff609bdceef2
imf-ali <alifahad2210@gmail.com>
2022-08-04 18:58:21 +0300

    init commmit

---
+++ helloPygame.py
@@ -0,0 +1,11 @@
+import pygame
+pygame.init()
+display = pygame.display.set_mode((640,480), 0, 32)
+pygame.display.set_caption("Hello Pygame")
+display.fill((0,0,0))
+game_over = False
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+pygame.quit()
---
+++ sprite_game.py
@@ -0,0 +1,15 @@
+from dis import dis
+import pygame
+pygame.init()
+display = pygame.display.set_mode((640,480), 0, 32)
+sprite1 = pygame.image.load('./images/football.png')
+pygame.display.set_caption("Hello Pygame")
+display.fill((0,0,0))
+game_over = False
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+    display.blit(sprite1, (0,0))
+    pygame.display.update()
+pygame.quit()
