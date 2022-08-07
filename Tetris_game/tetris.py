7b528745deb3701f10bbaf96af9c4dbe1bde561a
imf-ali <alifahad2210@gmail.com>
2022-08-07 10:56:52 +0300

    Tetris game

--- Tetris_game/tetris.py
+++ Tetris_game/tetris.py
@@ -1,30 +1,26 @@
 import pygame
 pygame.init()
 
-def drawGrid(screen, row, col, grid_size, x_gap, y_gap):
-    for y in range(row):
-            for x in range(col):
-                pygame.draw.rect(screen,(100,100,100),[x * grid_size + x_gap,y * grid_size + y_gap,grid_size,grid_size],1)
+def drawGrid(rows,cols,x_gap,y_gap,grid_size):
+    for y in range(rows):
+        for x in range(cols):
+            pygame.draw.rect(screen,(100,100,100),[x * grid_size + x_gap,y * grid_size + y_gap,grid_size,grid_size],1)
 
-def main():
-    grid_size = 30
+screen = pygame.display.set_mode((800,700))
+pygame.display.set_caption('Tetris')
+grid_size = 30
+cols = screen.get_width() // grid_size
+rows = screen.get_height() // grid_size
+x_gap = (screen.get_width() - cols * grid_size) // 2
+y_gap = (screen.get_height() - rows * grid_size) // 2
 
-    screen = pygame.display.set_mode((500,600))
-    pygame.display.set_caption("Tetris")
-    cols = screen.get_width() // grid_size
-    rows = screen.get_height() // grid_size
-    x_gap = (screen.get_width() - cols * grid_size) // 2
-    y_gap = (screen.get_height() - rows * grid_size) // 2
+game_over = False
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+    screen.fill((0,0,0))
+    drawGrid(rows,cols,x_gap,y_gap,grid_size)
+    pygame.display.update()
 
-    game_over = False
-    while not game_over:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game_over = True
-        screen.fill((0,0,0))
-        drawGrid(screen, rows,cols,grid_size, x_gap, y_gap)
-        pygame.display.update()
-    pygame.quit() 
-
-if __name__ == '__main__':
-    main()
+pygame.quit()
